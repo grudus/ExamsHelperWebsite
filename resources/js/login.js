516dca69152b91499352d027084ba5e0e1053dd8
grudus <grudus@protonmail.com>
2017-01-22 17:37:55 +0100

    cors problem solved

--- resources/js/login.js
+++ resources/js/login.js
@@ -1,9 +1,40 @@
-function init() {
-    const app = angular.module("ExamsHelperApp-login", []);
+angular.module("ExamsHelperApp-login", [])
+    .controller("appName", function ($scope) {
+        $scope.appName = "ExamsHelper";
+    })
+    .controller("singInForm", function ($scope, $http) {
+        var instance = this;
+        this.token = "";
 
-    app.controller("appName", function ($scope) {
-        $scope.appName="ExamsHelper"
+        $scope.tryToSignIn = function () {
+            if ($scope.username == "" || $scope.password == "") {
+                alert("Cannot login");
+            }
+            else {
+                $http({
+                    url: "http://localhost:8080/api/auth/login",
+                    method: "POST",
+                    headers: {withCredentials: true},
+                    params: {'username': $scope.username, 'password': $scope.password}
+                }).success(function (data, status, headers) {
+                    instance.token = headers()["x-auth-token"];
+                    instance.getInfo();
+                }).error(function (error) {
+                    console.log("error " + JSON.stringify(error));
+                })
+            }
+        };
+
+        this.getInfo = function () {
+            $http.get("http://localhost:8080/api/user", {headers: {"x-auth-token": instance.token}})
+                .success(function (data) {
+                    console.log(data);
+                })
+        }
+
+
     });
-}
 
-init();
+
+
+
