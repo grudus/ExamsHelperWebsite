e7daa33ace1a554e582b064b9c65dad3e648920c
grudus <jakgruda@gmail.com>
2017-04-17 00:41:12 +0200

    Basic AngularJS

---
+++ src/app/config.js
@@ -0,0 +1,14 @@
+/*@ngInject*/
+export default ($stateProvider, $urlRouterProvider, $httpProvider, $locationProvider, $translateProvider) => {
+    $locationProvider.html5Mode({enabled: true, requireBase: false});
+    $urlRouterProvider.otherwise('/');
+    $translateProvider.useLocalStorage().useLoaderCache(true).forceAsyncReload(true).useSanitizeValueStrategy(null)
+        .useStaticFilesLoader({
+            prefix: '/i18n/locale_',
+            suffix: '.json'
+        })
+        .registerAvailableLanguageKeys(["en"], {
+            "*": "en"
+        }).determinePreferredLanguage();
+
+}
--- src/app/index.js
+++ src/app/index.js
@@ -1,2 +1,25 @@
 import "../assets/sass/materialize.scss";
 import "./text.js"
+
+import angular from "angular";
+import uiRouter from "angular-ui-router";
+import translate from "angular-translate";
+import translateLocal from "angular-translate-storage-local";
+import translateCookie from "angular-translate-storage-cookie";
+import translateLoader from "angular-translate-loader-static-files";
+import angularCookies from "angular-cookies";
+
+import Config from "./config"
+
+angular.module("ExamsHelper", [
+    uiRouter,
+    angularCookies,
+    translate,
+    translateLocal,
+    translateCookie,
+    translateLoader
+]).config(Config);
+
+angular.element(document).ready(function () {
+    angular.bootstrap(document, ["ExamsHelper"], {strictDi: true});
+});
