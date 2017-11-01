7659b6a35dba7d97798c14df9a6ecd4d6f5d6cf3
grudus <jakgruda@gmail.com>
2017-11-01 21:47:59 +0100

    Deleting exams

--- src/app/exams/exams.service.js
+++ src/app/exams/exams.service.js
@@ -2,5 +2,6 @@
 export default ($resource, API) => $resource(API + '/exams/:resource/:label', {}, {
     getAllAsMap: {method: 'GET', isArray: true, params: {resource: "day"}},
     countWithoutGrade: {method: "GET", params: {resource: "without-grade", label: "count"}},
-    updateGrade: {method: 'PUT', params: {resource: '@id'}}
+    updateGrade: {method: 'PUT', params: {resource: '@id'}},
+    deleteExam: {method: 'DELETE', params: {resource: '@id'}}
 });
--- src/app/exams/upcoming/exams.upcoming.js
+++ src/app/exams/upcoming/exams.upcoming.js
@@ -2,15 +2,26 @@ require("./exams.upcoming.html")
 
 class UpcomingExams {
     /*@ngInject*/
-    constructor(ExamsService) {
+    constructor(ExamsService, $state) {
         this.ExamsService = ExamsService;
         this.loading = true;
+        this.$state = $state;
     }
 
     $onInit() {
         this.examsPerDay = this.examsPerDay
             .sort((a, b) => a.date.localeCompare(b.date));
         this.loading = false;
+    }
+
+    showDeleteModal(id) {
+        this.idToDelete = id;
+        this.showModal = true;
+    }
+
+    async deleteExam() {
+        await this.ExamsService.deleteExam({}, {id: this.idToDelete}).$promise;
+        this.$state.reload();
     }
 }
 
